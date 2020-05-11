5aa8f40bf469217dbda40f316a2b98554143ace6
Sam <hjiang129@gmail.com>
2020-05-11 10:15:04 -0400

    rename seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 import styled from "styled-components"
 
 import Layout from "../components/Layout"
-import SEO from "../components/SEO"
+import SEO from "../components/SiteSEO"
 
 const Container = styled.div`
   display: flex;
