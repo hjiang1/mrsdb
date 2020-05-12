14bad0ed2ea9c74495cc632ab850c63eafc3220a
Sam <hjiang129@gmail.com>
2020-05-12 19:44:40 -0400

    fix warnings

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -5,27 +5,34 @@ import cn from "classnames"
 
 const Container = styled.div`
   margin: 1rem 0;
-  display: flex;
-  align-items: center;
 
   .default-checkbox {
     display: none;
   }
 
-  .custom-checkbox {
+  .checkbox-label {
     display: flex;
-    justify-content: center;
     align-items: center;
-    cursor: pointer;
 
-    height: 1.5rem;
-    width: 1.5rem;
-    border-radius: 5px;
-    border: 2px solid darkgray;
-    margin-right: 0.5rem;
+    .custom-checkbox {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
 
-    &.checked {
-      background-color: var(--primaryColor);
+      height: 1.5rem;
+      width: 1.5rem;
+      border-radius: 5px;
+      border: 2px solid darkgray;
+      margin-right: 0.5rem;
+
+      &.checked {
+        background-color: var(--primaryColor);
+      }
+    }
+
+    .label-content {
+      cursor: pointer;
     }
   }
 `
@@ -40,13 +47,10 @@ const Checkbox = ({ checked, onChange, id, children }) => {
         checked={checked}
         onChange={() => onChange(id)}
       />
-      <div
-        className={cn("custom-checkbox", { checked })}
-        onClick={() => onChange(id)}
-      >
-        <FaCheck size="1rem" color="white" />
-      </div>
       <label className="checkbox-label" htmlFor={id}>
+        <div className={cn("custom-checkbox", { checked })}>
+          <FaCheck size="1rem" color="white" />
+        </div>
         <div className="label-content">{children}</div>
       </label>
     </Container>
